578662cb783b94cb191ea9269ada3a735a4655da
MartinMontagner <118780598+MartinMontagner@users.noreply.github.com>
2023-04-30 13:40:25 -0300

    Add files via upload

---
+++ parcialMontagner/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Main m= new Main();
+        m.saludar();
+
+    }
+    public void saludar()
+    {
+        System.out.println("Hello Parcial!");
+    }
+}
